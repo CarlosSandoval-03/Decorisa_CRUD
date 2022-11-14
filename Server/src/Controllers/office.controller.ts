bcadaddc328d07835951843e0b05d926daab97b1
Carlos Sandoval <csandovalc@unal.edu.co>
2022-11-13 21:31:33 -0500

    Operaciones base CRUD sobre sucursales

--- Server/src/Controllers/office.controller.ts
+++ Server/src/Controllers/office.controller.ts
@@ -19,18 +19,79 @@ export function getOffices (_req: Request, res: Response): Response {
   return res
 }
 
+export function getOfficeByName (req: Request, res: Response): Response {
+  const promiseConnect = connect(process.env.DB_TEST_USER ?? '', process.env.DB_TEST_PASSWORD ?? '')
+  const name = req.params.name
+
+  const promiseQuery = promiseConnect.then(async conn => {
+    const office = await conn.query('SELECT * FROM sucursal WHERE suc_nombre = ?', [name])
+    res.json(office[0])
+    res.status(200)
+  })
+
+  promiseQuery.catch(error => {
+    console.error(error)
+    res.status(500)
+  })
+
+  return res
+}
+
 export function createOffice (req: Request, res: Response): Response {
   const newOffice: Office = req.body
 
   const promiseConnect = connect(process.env.DB_TEST_USER ?? '', process.env.DB_TEST_PASSWORD ?? '')
 
   const promiseQuery = promiseConnect.then(async conn => {
-    const data = await conn.query(`INSERT INTO sucursal VALUES ('${newOffice.suc_direccion}', '${newOffice.suc_nombre}')`)
+    const data = await conn.query('INSERT INTO sucursal SET ?', [newOffice])
 
     res.json({
       message: 'New entry in Sucursal',
       sucursal_creada: newOffice,
       state: data[0]
+    })
+    res.status(200)
+  })
+
+  promiseQuery.catch(error => {
+    console.error(error)
+    res.status(500)
+  })
+
+  return res
+}
+
+export function deleteOffice (req: Request, res: Response): Response {
+  const promiseConnect = connect(process.env.DB_TEST_USER ?? '', process.env.DB_TEST_PASSWORD ?? '')
+  const name = req.params.name
+
+  const promiseQuery = promiseConnect.then(async conn => {
+    const office = await conn.query('DELETE FROM sucursal WHERE suc_nombre = ?', [name])
+    res.json({
+      message: `Entry '${name}' deleted`,
+      sqlReport: office[0]
+    })
+    res.status(200)
+  })
+
+  promiseQuery.catch(error => {
+    console.error(error)
+    res.status(500)
+  })
+
+  return res
+}
+
+export function updateOffice (req: Request, res: Response): Response {
+  const promiseConnect = connect(process.env.DB_TEST_USER ?? '', process.env.DB_TEST_PASSWORD ?? '')
+  const name = req.params.name
+  const newValueOffice: Office = req.body
+
+  const promiseQuery = promiseConnect.then(async conn => {
+    const office = await conn.query('UPDATE sucursal SET ? WHERE suc_nombre = ?', [newValueOffice, name])
+    res.json({
+      message: `Entry '${name}' updated`,
+      sqlReport: office[0]
     })
     res.status(200)
   })
--- Server/src/Routes/office.routes.ts
+++ Server/src/Routes/office.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express'
 
-import { getOffices, createOffice } from '../Controllers/office.controller'
+import { getOffices, getOfficeByName, createOffice, deleteOffice, updateOffice } from '../Controllers/office.controller'
 
 const router = Router()
 
 router.route('/')
   .get(getOffices)
   .post(createOffice)
+
+router.route('/:name')
+  .get(getOfficeByName)
+  .delete(deleteOffice)
+  .put(updateOffice)
 
 export default router
