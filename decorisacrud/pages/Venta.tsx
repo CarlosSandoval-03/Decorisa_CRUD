335f4a02a95103314a0ea15937ece3ef36017800
Juanda <jupalaciosf@unal.edu.co>
2022-11-15 17:58:26 -0500

    Pagina ventas

--- decorisacrud/pages/Venta.tsx
+++ decorisacrud/pages/Venta.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
 
 function Venta() {
   return (
-    <div>Venta</div>
+    <>
+      <Navbar></Navbar>
+      <div className='ventas'>
+        <div className='ventas-container'>
+          <h1>Ventas realizadas este mes</h1>
+        </div>
+        <div className='ventas-container'>
+          <h1>Total vendido</h1>
+        </div>
+       
+      </div>
+      
+    </>
+    
   )
 }
 
